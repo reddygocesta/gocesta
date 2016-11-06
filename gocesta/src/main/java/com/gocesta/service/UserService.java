cf569d69ccbb4578d66a0b5cfec393c04a839617
reddygocesta <narasimhareddy@gocesta.com>
2016-11-06 20:17:45 +0530

    initial commit

---
+++ gocesta/src/main/java/com/gocesta/service/UserService.java
@@ -0,0 +1,27 @@
+package com.gocesta.service;
+
+import java.util.List;
+
+import com.gocesta.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
